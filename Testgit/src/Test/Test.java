db745559be9d961ce6b9d860cd7d2b0018c12ed5
UPPI.GIT <upendertadewar.wmb@gmail.com>
2019-02-01 23:00:11 +0530

    changes in test

--- Testgit/src/Test/Test.java
+++ Testgit/src/Test/Test.java
@@ -4,11 +4,15 @@ public class Test {
 	
 	static int a=10;
 	static int b=20;
+	int c =1000;
+	int d=800;
 	public static void main(String[] args) {
+		Test t = new Test();
 		System.out.println(a);
 		System.out.println(b);
+		System.out.println(t.c);
+		System.out.println(t.d);
 		
-		Test t = new Test();
 		t.M1();
 	}
 	
@@ -16,6 +20,8 @@ public class Test {
 	{
 		System.out.println(a);
 		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
 	}
 
 }
